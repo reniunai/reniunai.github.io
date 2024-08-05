355827e3772f780e9ae34ec10b75296d844d7afa
reniunai <2843768@qq.com>
2024-08-05 19:11:27 +0800

    修改

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -10,7 +10,12 @@ export default defineUserConfig({
   description: "学习笔记",
 
   theme,
-
+  markdown: {
+    headers: {
+      // 用到哪一级就提取哪一级
+      level: [1, 2, 3],
+    },
+  },
   // 和 PWA 一起启用
   // shouldPrefetch: false,
 });
